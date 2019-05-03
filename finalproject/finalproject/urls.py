0a331b9f236f373b1beb96f8eb098e3ab3f3b4dd
John Lahut <lahut.john@gmail.com>
2019-05-02 21:58:26 -0400

    as complete as it is going to get! ✅

--- finalproject/finalproject/urls.py
+++ finalproject/finalproject/urls.py
@@ -20,7 +20,7 @@ from . import views
 
 urlpatterns = [
     # path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('maker/', include('maker.urls', namespace='maker')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
